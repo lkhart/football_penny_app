bda4979d71b8cfd6fafccaead5133dd870dc5744
lkhart <97354054+lkhart@users.noreply.github.com>
2023-10-12 18:05:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-# Custom Theme for the company's colors
 st.set_page_config(
     page_title="Penny: Football Play Analysis Tool",
     layout="wide",
@@ -10,47 +9,55 @@ st.set_page_config(
 # Streamlit App
 st.title("Penny: Football Play Analysis Tool")
 
-# User input for offensive players' routes
-num_receivers = st.slider("Number of receivers running routes on offense:", 1, 10)
-num_defenders = st.slider("Number of defenders involved in coverage:", 1, 10)
+# Create two columns
+left_column, right_column = st.beta_columns(2)
 
+# Inputs in the left column
+
+# User input for offensive players' routes
+num_receivers = int(left_column.text_input("Number of receivers running routes on offense:", "1"))
+num_defenders = int(left_column.text_input("Number of defenders involved in coverage:", "1"))
+
+left_column.subheader("Offensive Routes")
 routes = {}
 for i in range(num_receivers):
-    route = st.text_input(f"Route for offensive player {i+1}:")
+    route = left_column.text_input(f"Route for offensive player {i+1}:")
     routes[f"Player {i+1}"] = route
 
-# Speed ratings for each player
-st.subheader("Speed Ratings for Players")
+left_column.subheader("Speed Ratings for Players")
 offensive_speeds = {}
-defensive_speeds = {}
 for i in range(num_receivers):
-    speed = st.slider(f"Speed rating for offensive player {i+1}:", 0, 100)
+    speed = int(left_column.text_input(f"Speed rating for offensive player {i+1}:", "0"))
     offensive_speeds[f"Player {i+1}"] = speed
 
+defensive_speeds = {}
 for i in range(num_defenders):
-    speed = st.slider(f"Speed rating for defensive player {i+1}:", 0, 100)
+    speed = int(left_column.text_input(f"Speed rating for defensive player {i+1}:", "0"))
     defensive_speeds[f"Defender {i+1}"] = speed
 
 # Weight assignment for defensive positioning
-weight = st.slider("Weight for defensive player positioning:", 0, 100)
-
-# Button to run the simulation
-if st.button("Run Simulation"):
-    st.write("Running the simulation... (this is a placeholder, you'll need to implement the actual simulation)")
-
-# Display the results (GIF and play art form)
-st.subheader("Simulation Results")
-st.write("GIF result (placeholder)")
-st.image("https://via.placeholder.com/300")  # Placeholder, replace with actual GIF
-st.write("Play art result (placeholder)")
-st.image("https://via.placeholder.com/300")  # Placeholder, replace with actual play art
+weight = int(left_column.text_input("Weight for defensive player positioning:", "0"))
 
 # Interface for analyzing NFL play-by-play data
-st.subheader("NFL Play-by-Play Analysis")
-team = st.selectbox("Select NFL Team:", ["Team A", "Team B", "Team C"], index=0)  # Placeholder team names
-week = st.selectbox("Select Week:", list(range(1, 18)))
+left_column.subheader("NFL Play-by-Play Analysis")
+team = left_column.selectbox("Select NFL Team:", ["Team A", "Team B", "Team C"], index=0)  # Placeholder team names
+week = left_column.selectbox("Select Week:", list(range(1, 18)))
 
-if st.button("Fetch and Analyze Data"):
-    st.write(f"Fetching data for {team} for week {week}... (this is a placeholder, you'll need to connect to your database and fetch the actual data)")
+# Results in the right column
+
+# Button to run the simulation
+if right_column.button("Run Simulation"):
+    right_column.write("Running the simulation... (this is a placeholder, you'll need to implement the actual simulation)")
+
+# Display the simulation results
+right_column.subheader("Simulation Results")
+right_column.write("GIF result (placeholder)")
+right_column.image("https://via.placeholder.com/300")  # Placeholder, replace with actual GIF
+right_column.write("Play art result (placeholder)")
+right_column.image("https://via.placeholder.com/300")  # Placeholder, replace with actual play art
+
+if right_column.button("Fetch and Analyze Data"):
+    right_column.write(f"Fetching data for {team} for week {week}... (this is a placeholder, you'll need to connect to your database and fetch the actual data)")
+
 
 
